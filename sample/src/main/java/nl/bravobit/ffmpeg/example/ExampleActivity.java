686c4b50653e39c5acb181b0d09cec03e873af5d
Alex Cohn <sasha.cohn@gmail.com>
2020-02-09 01:43:40 +0200

    use the Storage Access Framework for input
    Work around the API 29 new [restrictions on use of external storage](https://developer.android.com/training/data-storage/?hl=en#scoped-storage). For brevity, we simply use the ACTION_OPEN_DOCUMENT intent here.

--- sample/src/main/java/nl/bravobit/ffmpeg/example/ExampleActivity.java
+++ sample/src/main/java/nl/bravobit/ffmpeg/example/ExampleActivity.java
@@ -14,7 +14,9 @@ import timber.log.Timber;
  * Created by Brian on 11-12-17.
  */
 public class ExampleActivity extends AppCompatActivity {
+    private static final int MOVIE_OPEN_REQUEST = 1;
     private Handler handler = new Handler();
+    private String mediaFile;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -114,4 +116,59 @@ public class ExampleActivity extends AppCompatActivity {
             }
         }, 8000);
     }
+    
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (mediaFile == null) {
+            startActivityForResult(new Intent(Intent.ACTION_OPEN_DOCUMENT)
+                    .setType("video/*")
+                    .putExtra("android.content.extra.SHOW_ADVANCED", true) // thanks Commonsware, https://issuetracker.google.com/issues/72053350
+                    .putExtra("android.content.extra.FANCY", true)
+                    .putExtra("android.content.extra.SHOW_FILESIZE", true)
+                    .addCategory(Intent.CATEGORY_OPENABLE), DOC_OPEN_REQUEST);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == MOVIE_OPEN_REQUEST && resultCode == RESULT_OK && data != null) {
+            Uri uri = data.getData();
+            try {
+                ParcelFileDescriptor parcelFileDescriptor = getContentResolver().openFileDescriptor(uri, "r");
+                int fd = parcelFileDescriptor.getFd();
+                int pid = Process.myPid();
+                mediaFile = "/proc/" + pid + "/fd/" + fd;
+                Timber.i("opened " + uri + " as " + mediaFile);
+                runFFmpeg();
+            } catch (Throwable e) {
+                Timber.e(e, "cannot work with " + uri);
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+    
+    private void runFFmpeg() {
+        String outPath = getExternalFilesDir(null) + "/out.mp4"
+        new File(outPath).delete();
+        FFmpeg.getInstance(this).execute(new String[]{"-i", mediaFile, "-frames:v", "500", outPath}, new ExecuteBinaryResponseHandler() {
+            @Override
+            public void onSuccess(String message) {
+                Timber.e(message.substring(0, 100));
+            }
+
+            @Override
+            public void onProgress(String message) {
+                Timber.i(message);
+            }
+
+            @Override
+            public void onFailure(String message) {
+                Timber.e(message);
+            }
+        });
+    }
+
 }
